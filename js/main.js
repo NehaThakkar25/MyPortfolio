efbda434383b57802546cfdfa34014b1b7090798
NehaThakkar25 <nehathakkar3008@gmail.com>
2021-05-25 15:41:58 +0530

    dark light theme

--- js/main.js
+++ js/main.js
@@ -88,12 +88,50 @@ function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
-        const scetionHeight = current.offsetHeight
+        const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionID = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href+=' + sectionID + ']').classList;
+            document.querySelector('.nav_menu a[href*=' + sectionID + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav_menu a[href*=' + sectionID + ']').classList.remove('active-link')
         }
     })
 }
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80) 
+        nav.classList.add('scroll-header');
+    else
+        nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon ===  'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () =>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
